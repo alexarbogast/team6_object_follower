e44fca63c77823c9a2dc4f9e64a2fbcc4a337ffd
csevvy <89365461+csevvy@users.noreply.github.com>
2021-09-17 18:37:49 -0400

    Robot rotate now subscribes to target location and turns toward it

--- scripts/rotate_robot.py
+++ scripts/rotate_robot.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python
 import sys
-
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point
 
 BURGER_MAX_ANG_VEL = 2.84
+center_of_image_x=200
+
+twist = Twist()
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+def rot_vel(error):
+	max_v=BURGER_MAX_ANG_VEL
+	max_error_magnitude=center_of_image_x
+	vel=-(max_v/max_error_magnitude)*error	#linear interpolation
+	return vel
+
+def callback(data):
+	if data is None:
+		twist.angular.z=0.0
+
+	else:
+		x=data.x; data.y; data.z		#need to double check that x is horizontal
+		target_error=x-center_of_image_x #negative == turn left, positive == turn right
+		vel_out=rot_vel(target_error)
+		twist.angular.z=vel_out
+
+	pub.publish(twist)	
+	print(data)	
 
 if __name__=='__main__':
 	rospy.init_node('rotate_robot', anonymous=True)
+	sub = rospy.Subscriber('target_loc', Point, callback,queue_size=10)
+	rospy.spin()
 
-	pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-	twist = Twist()
-	twist.angular.x = 0.0; twist.angular.y = 0.0; 
-
-	try:
-		while not rospy.is_shutdown():
-			twist.angular.z = BURGER_MAX_ANG_VEL
-			pub.publish(twist)
-
-	except:
-		print("Unexpected error:", sys.exc_info()[0])
-
-	finally:
-	    twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
-	    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
-	    pub.publish(twist)
+	#try:
+	#	while not rospy.is_shutdown():
+	#		#twist.angular.z = BURGER_MAX_ANG_VEL
+	#		#twist.angular.z=vel_out
+#
+	#except:
+	#	print("Unexpected error:", sys.exc_info()[0])
+#
+	#finally:
+	#    twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+	#    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+	#    pub.publish(twist)
