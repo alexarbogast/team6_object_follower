29981a06884b4283700a559df246a0e24edc405c
Alex Arbogast <arbogastaw@gmail.com>
2021-10-18 19:18:26 -0400

    fix incorrect coordinate transformations for waypoints

--- team6_chase_object/src/pid/pid.py
+++ team6_chase_object/src/pid/pid.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
 class PID:
-	def __init__(self, Ki, Kp, Kd, max_out = None, min_out = None):
-		self.SetGains(Ki, Kp, Kd)
-		self._max_out, self._min_out = max_out, min_out
+	def __init__(self, Kp, Ki, Kd):
+		self.SetGains(Kp, Ki, Kd)
 
 		self._error_sum, self._prv_error = 0, 0
 
-	def SetGains(self, Ki, Kp, Kd): 
-		if (not((Ki <= 0 and Kp <= 0 and Kd <= 0)
-			or (Ki >= 0 and Kp >=0 and Kd >= 0))):
+	def SetGains(self, Kp, Ki, Kd): 
+		if (not((Kp <= 0 and Ki <= 0 and Kd <= 0)
+			or (Kp >= 0 and Ki >=0 and Kd >= 0))):
 			print('Warning: Gains should have the same sign for stability')
 
-		self._Ki, self._Kp, self._Kd = Ki, Kp, Kd
+		self._Kp, self._Ki, self._Kd = Kp, Ki, Kd
 
 	def Calculate(self, dt, setpoint, plant_state):
 		error = setpoint - plant_state
@@ -25,9 +24,14 @@ class PID:
 		self._prv_error = error
 		control_out = prop_error + intg_error + derv_error
 
-		if (self._max_out is not None) and control_out >= self._max_out:
-			control_out = self._max_out
-		elif (self._min_out is not None) and control_out <= self._min_out:
-			control_out = self._min_out
-
 		return control_out
+
+	@staticmethod
+	def Saturate(output, max_out, min_out):
+		if output > max_out:
+			output = max_out
+		elif output < min_out:
+			output = min_out
+
+		return output
+
--- team6_navigate_to_goal/scripts/go_to_goal.py
+++ team6_navigate_to_goal/scripts/go_to_goal.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 ############################################################
-# go_to_goal:
+# go_to_goal node: moves the turtle bot to waypoints 
+# while avoiding obstacles 
 ############################################################
 
 import rospy
@@ -10,12 +11,17 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist, Pose2D
 
+from pid import PID
+
 import numpy as np
 from enum import Enum
 
 # constants
 OBST_THRESH = 0.2 # m
 WAYPOINT_THRESH = 0.01 # m
+
+BURGER_MAX_ANG_VEL = 2.84
+BURGER_MAX_LIN_VEL = 0.22
 
 class State(Enum):
 	GoToGoal = 1
@@ -28,20 +34,21 @@ class Waypoint:
 	def __str__(self):
 		return "({0}, {1})".format(self.x, self.y)
 
-	# get waypoint heading in degrees
+	# get waypoint heading in radians
 	def GetHeading(self):
-		return np.degrees(np.arctan2(self.y, self.x))
+		return np.arctan2(self.y, self.x)
 
 	def GetDistance(self):
 		return np.sqrt(self.x*self.x + self.y*self.y)
 
 class Odom:
 	def __init__(self):
-		self._global_pose = Pose2D 
-		self._init_pose = Pose2D
+		self._global_pose = Pose2D()
+		self._init_pose = Pose2D()
 		self._init = True
 
 		self._odom_sub = rospy.Subscriber('/odom', Odometry, self.UpdateOdometry, queue_size=10)
+		self._odom_pub = rospy.Publisher('/debug_odom', Pose2D, queue_size=10)
 
 	def ResetOdom(self, Odom):
 		position = Odom.pose.pose.position
@@ -73,8 +80,10 @@ class Odom:
 						   np.cos(self._init_pose.theta)]])
 
 		self._global_pose.x = Mrot.item((0,0))*position.x + Mrot.item((0,1))*position.y - self._init_pose.x
-		self._global_pose.y = Mrot.item((1,0))*position.x + Mrot.item((1,1))*position.y
+		self._global_pose.y = Mrot.item((1,0))*position.x + Mrot.item((1,1))*position.y - self._init_pose.y
 		self._global_pose.theta = orientation - self._init_pose.theta
+
+		self._odom_pub.publish(self._global_pose)
 
 	def GetOdometry(self):
 		return self._global_pose
@@ -84,25 +93,66 @@ class GoToGoal:
 		self._waypoints = waypoints
 		self._thresh = waypoint_thresh
 
-	# get the current waypoint in the robot frame
+		self._ang_controller =  PID(1.7, 0, 0)
+		self._dist_controller = PID(1, 0, 0.0)
+
 	def GetCurrentWaypointRf(self, odom):
 		waypoint = self._waypoints[0]
 		waypoint_rf = Waypoint()
 
 		x, y, theta  = odom.x, odom.y, odom.theta
-		Mrot = np.matrix([[np.cos(theta), -np.sin(theta)],
-						  [np.sin(theta),  np.cos(theta)]])
 
-		waypoint_rf.x, waypoint_rf.y = waypoint.x - x, waypoint_rf.y - y
+		# world frame to robot frame rotation
+		Mrot = np.matrix([[np.cos(theta), np.sin(theta)],
+						 [-np.sin(theta), np.cos(theta)]])
 
-		# rotate waypoint
-		waypoint_rf.x = Mrot.item((0,0))*waypoint_rf.x + Mrot.item((0,1))*waypoint_rf.y
-		waypoint_rf.y = Mrot.item((1,0))*waypoint_rf.x + Mrot.item((1,1))*waypoint_rf.y
+		# vec from robot to waypoint in world frame
+		rob_to_wp_x_wf = waypoint.x - x
+		rob_to_wp_y_wf = waypoint.y - y
+
+		# rotate to robot frame
+		waypoint_rf.x = Mrot.item((0,0))*rob_to_wp_x_wf + Mrot.item((0,1))*rob_to_wp_y_wf
+		waypoint_rf.y = Mrot.item((1,0))*rob_to_wp_x_wf+ Mrot.item((1,1))*rob_to_wp_y_wf
 
 		return waypoint_rf
 
 	def Control(self, odom):
-		return Twist()
+		ang_vel, lin_vel = 0, 0
+
+		waypoint_rf = self.GetCurrentWaypointRf(odom)
+		wp_heading = waypoint_rf.GetHeading()
+
+		test_angle_thresh = 0.05
+
+		if (waypoint_rf.GetDistance() < self._thresh):
+			self._waypoints.pop(0)
+			print('waypoint reached\n')
+
+		elif (wp_heading > test_angle_thresh):
+			ang_vel = self._ang_controller.Calculate(0.01, wp_heading, 0)
+			print('going for angle\n')
+
+		else: # control position (robot frame)
+			ang_vel = self._ang_controller.Calculate(0.01, wp_heading, 0)
+			lin_vel = self._dist_controller.Calculate(0.01, waypoint_rf.x, 0)
+			print('going for position\n')
+		
+		ang_vel = PID.Saturate(ang_vel, BURGER_MAX_ANG_VEL, -BURGER_MAX_ANG_VEL)
+		lin_vel = PID.Saturate(lin_vel, BURGER_MAX_LIN_VEL, -BURGER_MAX_LIN_VEL)
+			
+		control_output = Twist()
+		control_output.angular.z, control_output.linear.x = ang_vel, lin_vel
+
+		# debug output
+		print("---------------")
+		print("Waypoint RF = {0}".format(waypoint_rf))
+		print("Odom theta = {0}".format(np.degrees(odom.theta)))
+		print("Heading = {0}\n".format(np.degrees(wp_heading)))
+
+		print("Angular Vel = {0}".format(ang_vel))
+		print("Linear Vel = {0}".format(lin_vel))
+
+		return control_output
 
 class AvoidObstacle:
 	def __init__(self):
@@ -139,20 +189,18 @@ class NavController:
 	def FindState(self, lidar_data):
 		# TODO: parse lidar data to find if an obstacle
 		# obstructs path to goal
-		
 		waypoint_rf = self._go_to_goal.GetCurrentWaypointRf(self._odom.GetOdometry())
 
 		# find heading of current waypoint in robot frame
 		heading = waypoint_rf.GetHeading()
 		distance = waypoint_rf.GetDistance()
 
+		# temp place holder
 		min_lidar_dist = 0.3
 
 		state = State.GoToGoal
 		if min_lidar_dist < self._obstacle_thresh:
 			state = State.AvoidObstacle
-		else:
-			state = State.GoToGoal
 
 		return state
 
