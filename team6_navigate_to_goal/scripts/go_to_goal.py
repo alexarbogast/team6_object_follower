018b0aaaa0abd3cc37317283e8b827e20eb12781
Alex Arbogast <arbogastaw@gmail.com>
2021-10-18 10:37:06 -0400

    add functionality to Waypoint and GoToGoal classes

--- team6_navigate_to_goal/scripts/go_to_goal.py
+++ team6_navigate_to_goal/scripts/go_to_goal.py
@@ -14,15 +14,26 @@ import numpy as np
 from enum import Enum
 
 # constants
-OBST_THRESH = 0.2 #m
+OBST_THRESH = 0.2 # m
+WAYPOINT_THRESH = 0.01 # m
 
 class State(Enum):
 	GoToGoal = 1
 	AvoidObstacle = 2 
 
 class Waypoint:
-	def __init__(self, x, y):
-		self.X, self.Y = x, y
+	def __init__(self, x=0, y=0):
+		self.x, self.y = x, y
+
+	def __str__(self):
+		return "({0}, {1})".format(self.x, self.y)
+
+	# get waypoint heading in degrees
+	def GetHeading(self):
+		return np.degrees(np.arctan2(self.y, self.x))
+
+	def GetDistance(self):
+		return np.sqrt(self.x*self.x + self.y*self.y)
 
 class Odom:
 	def __init__(self):
@@ -69,55 +80,82 @@ class Odom:
 		return self._global_pose
 
 class GoToGoal:
-	def __init__(self, waypoints):
+	def __init__(self, waypoints, waypoint_thresh=0.01):
 		self._waypoints = waypoints
+		self._thresh = waypoint_thresh
 
-	def control(self):
-		pass
+	# get the current waypoint in the robot frame
+	def GetCurrentWaypointRf(self, odom):
+		waypoint = self._waypoints[0]
+		waypoint_rf = Waypoint()
+
+		x, y, theta  = odom.x, odom.y, odom.theta
+		Mrot = np.matrix([[np.cos(theta), -np.sin(theta)],
+						  [np.sin(theta),  np.cos(theta)]])
+
+		waypoint_rf.x, waypoint_rf.y = waypoint.x - x, waypoint_rf.y - y
+
+		# rotate waypoint
+		waypoint_rf.x = Mrot.item((0,0))*waypoint_rf.x + Mrot.item((0,1))*waypoint_rf.y
+		waypoint_rf.y = Mrot.item((1,0))*waypoint_rf.x + Mrot.item((1,1))*waypoint_rf.y
+
+		return waypoint_rf
+
+	def Control(self, odom):
+		return Twist()
 
 class AvoidObstacle:
 	def __init__(self):
 		pass
 
-	def control(self):
-		pass
+	def Control(self):
+		return Twist()
 
 class NavController:
-	def __init__(self, waypoints,  obstacle_thresh=0.1):
-		self._state = State.GoToGoal
+	def __init__(self, waypoints,  obstacle_thresh=0.1, waypoint_thresh=0.01):
 		self._obstacle_thresh = obstacle_thresh
 
 		self._twist = Twist()
-		self._odom = Odometry()
 
 		self._odom = Odom()
-		self._go_to_goal = GoToGoal(waypoints)
+		self._go_to_goal = GoToGoal(waypoints, waypoint_thresh)
 		self._avoid_obstacle = AvoidObstacle()
 
-		self.lidar_sub = rospy.Subscriber("/scan", LaserScan, self.LidarCallback, queue_size=1)
+		self.lidar_sub = rospy.Subscriber("/scan", LaserScan, self.LidarCallback, queue_size=10)
 		self._vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size = 10)
 
 		rospy.on_shutdown(self.ShutdownNav)
 
 	def LidarCallback(self, lidar_data):
-		self.FindState(lidar_data)
+		state = self.FindState(lidar_data)
 
-		if self._state == State.GoToGoal:
-			self._twist = self._go_to_goal.control()
+		if state == State.GoToGoal:
+			self._twist = self._go_to_goal.Control(self._odom.GetOdometry())
 		else:
-			self._twist = self._avoid_obstacle.control()
+			self._twist = self._avoid_obstacle.Control()
 
 		self._vel_pub.publish(self._twist)
 
 	def FindState(self, lidar_data):
-		# TODO: parse lidar data to find min dist
+		# TODO: parse lidar data to find if an obstacle
+		# obstructs path to goal
+		
+		waypoint_rf = self._go_to_goal.GetCurrentWaypointRf(self._odom.GetOdometry())
+
+		# find heading of current waypoint in robot frame
+		heading = waypoint_rf.GetHeading()
+		distance = waypoint_rf.GetDistance()
+
 		min_lidar_dist = 0.3
 
+		state = State.GoToGoal
 		if min_lidar_dist < self._obstacle_thresh:
-			self._state = State.AvoidObstacle
+			state = State.AvoidObstacle
 		else:
-			self._state = State.GoToGoal
-	
+			state = State.GoToGoal
+
+		return state
+
 	# shutdown hook
 	def ShutdownNav(self):
 		self._twist.angular.x, self._twist.angular.y, self._twist.angular.z = 0.0, 0.0, 0.0
@@ -136,7 +174,7 @@ if __name__=='__main__':
 
 	waypoints = [point1, point2, point3]
 
-	state_controller = NavController(waypoints, OBST_THRESH)
+	state_controller = NavController(waypoints, OBST_THRESH, WAYPOINT_THRESH)
 
 	try:
 		rospy.spin()
