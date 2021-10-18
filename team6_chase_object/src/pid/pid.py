30ddb1cf8f2b35ee1086922aef39a511f60318a6
Alex Arbogast <arbogastaw@gmail.com>
2021-10-18 11:30:17 -0400

    add control output bounds for PID

--- team6_chase_object/src/pid/pid.py
+++ team6_chase_object/src/pid/pid.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 class PID:
-	def __init__(self, Ki, Kp, Kd):
+	def __init__(self, Ki, Kp, Kd, max_out = None, min_out = None):
 		self.SetGains(Ki, Kp, Kd)
+		self._max_out, self._min_out = max_out, min_out
+
 		self._error_sum, self._prv_error = 0, 0
 
 	def SetGains(self, Ki, Kp, Kd): 
-		if (not(Ki <= 0 or Kp <= 0 or Kd <= 0)
-			or (Ki >= 0 or Kp >=0 or Kd >= 0)):
+		if (not((Ki <= 0 and Kp <= 0 and Kd <= 0)
+			or (Ki >= 0 and Kp >=0 and Kd >= 0))):
 			print('Warning: Gains should have the same sign for stability')
 
 		self._Ki, self._Kp, self._Kd = Ki, Kp, Kd
@@ -21,4 +23,11 @@ class PID:
 		derv_error = self._Kd*(error-self._prv_error)/dt
 
 		self._prv_error = error
-		return prop_error + intg_error + derv_error
+		control_out = prop_error + intg_error + derv_error
+
+		if (self._max_out is not None) and control_out >= self._max_out:
+			control_out = self._max_out
+		elif (self._min_out is not None) and control_out <= self._min_out:
+			control_out = self._min_out
+
+		return control_out
